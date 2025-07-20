e2b64e58321c35f57066788382d2e2716e905dba
JLPM22 <joseluispm2@gmail.com>
2025-07-20 19:31:58 +0200

    character rotation

--- pymm.py
+++ pymm.py
@@ -1,12 +1,14 @@
 import argparse
+import numpy as np
+import pymotion.rotations.quat as quat
 
 from pyray import *
 from raylib import CAMERA_ORBITAL, CAMERA_PERSPECTIVE
 from src.data_utils import load_data
 
 
-def main(data_dir, scale):
-    pose_db, features_db = load_data(data_dir, scale)
+def main(data_dir, scale, local_forward_hips):
+    pose_db, features_db = load_data(data_dir, scale, local_forward_hips)
 
     # --- Window and Camera Setup ---
     screen_width = 1280
@@ -26,8 +28,8 @@ def main(data_dir, scale):
     set_target_fps(60)
 
     # --- Animation State ---
-    frame_index = -1
-    num_frames = pose_db.shape[0]
+    frame_index = 1000
+    num_frames = len(pose_db)
 
     # --- Main Animation Loop ---
     while not window_should_close():
@@ -37,7 +39,7 @@ def main(data_dir, scale):
         frame_index = (frame_index + 1) % num_frames
 
         # Get the root joint's position for the current frame (usually the first joint)
-        root_joint_pos = pose_db[frame_index, 0]
+        root_joint_pos = pose_db.poses[frame_index, 0]
 
         # UPDATE THE CAMERA TARGET TO FOLLOW THE ROOT JOINT
         camera.target = Vector3(root_joint_pos[0], root_joint_pos[1], root_joint_pos[2])
@@ -52,11 +54,22 @@ def main(data_dir, scale):
         begin_mode_3d(camera)
 
         # Draw the pose for the current frame
-        current_pose = pose_db[frame_index]
+        current_pose = pose_db.poses[frame_index]
         for joint_position in current_pose:
             # Create a Vector3 from the joint's [x, y, z] coordinates and scale it down
             pos_vec = Vector3(joint_position[0], joint_position[1], joint_position[2])
             draw_sphere(pos_vec, 0.05, MAROON)  # Draw a small sphere at the joint's position
+        character_rot = pose_db.character_rot[frame_index]
+        character_forward = quat.mul_vec(character_rot, np.array([0.0, 0.0, 1.0]))
+        draw_line_3d(
+            Vector3(root_joint_pos[0], root_joint_pos[1], root_joint_pos[2]),
+            Vector3(
+                root_joint_pos[0] + character_forward[0],
+                root_joint_pos[1] + character_forward[1],
+                root_joint_pos[2] + character_forward[2],
+            ),
+            BLUE,
+        )
 
         draw_grid(20, 1.0)  # Draw a ground plane for reference
 
@@ -76,7 +89,13 @@ if __name__ == "__main__":
     parser.add_argument(
         "--data_dir", type=str, default="data", help="Directory where the .bvh files are stored"
     )
-    parser.add_argument("--scale", type=float, default=0.01, help="Scale factor for the animation")
+    parser.add_argument("--scale", type=list, default=0.01, help="Scale factor for the animation")
+    parser.add_argument(
+        "--local_forward_hips",
+        type=tuple,
+        default=[0.0, 0.11043152, 0.9938837],
+        help="Local forward vector for hips",
+    )
     args = parser.parse_args()
 
-    main(args.data_dir, args.scale)
+    main(args.data_dir, args.scale, args.local_forward_hips)
--- src/data_utils.py
+++ src/data_utils.py
@@ -1,11 +1,25 @@
 import os
 import numpy as np
+import pymotion.rotations.quat as quat
+import pymotion.ops.vector as vec
 
 from pymotion.io.bvh import BVH
 from pymotion.ops.skeleton import fk
 
 
-def load_data(data_dir, scale):
+class PoseDB:
+    def __init__(self, poses, character_rot):
+        self.poses = poses
+        self.character_rot = character_rot
+
+    def __getitem__(self, index):
+        return self.poses[index], self.character_rot[index]
+
+    def __len__(self):
+        return len(self.poses)
+
+
+def load_data(data_dir, scale, local_forward_hips):
     animations = []
     for filename in os.listdir(data_dir):
         if filename.endswith(".bvh"):
@@ -15,21 +29,41 @@ def load_data(data_dir, scale):
             bvh.set_scale(scale)
             animations.append(bvh)
 
-    pose_db = create_pose_db(animations)
+    pose_db = create_pose_db(animations, local_forward_hips)
     features_db = create_features_db(pose_db)
 
     return pose_db, features_db
 
 
-def create_pose_db(animations):
+def create_pose_db(animations, local_forward_hips):
     poses = []
+    character_rots = []
     for animation in animations:
         local_rotations, local_positions, parents, offsets, _, _ = animation.get_data()
-        global_positions = local_positions[:, 0, :]  # root joint
+        global_rotations = local_rotations[:, 0, :]
+        global_positions = local_positions[:, 0, :]
         positions, _ = fk(local_rotations, global_positions, offsets, parents)
+
+        # Add character space joint
+        positions = np.concatenate([positions[:, 0:1, :], positions], axis=1)
+        positions[:, 0, 1] = 0.0  # Set character space joint Y to 0 for ground level
+
+        forward_hips = quat.mul_vec(global_rotations, np.array(local_forward_hips))
+        forward_hips[:, 1] = 0.0  # Set Y component to 0 for ground level
+        forward_hips = vec.normalize(forward_hips)
+        character_rot = quat.from_to_axis(
+            np.tile(np.array([0.0, 0.0, 1.0]), forward_hips.shape[:-1] + (1,)),
+            forward_hips,
+            rot_axis=np.tile(np.array([0.0, 1.0, 0.0]), forward_hips.shape[:-1] + (1,)),
+            normalize_input=False,
+        )
+
         poses.append(positions)
+        character_rots.append(character_rot)
     poses = np.concatenate(poses, axis=0)
-    return poses
+    character_rots = np.concatenate(character_rots, axis=0)
+    pose_db = PoseDB(poses, character_rots)
+    return pose_db
 
 
 def create_features_db(pose_db):
